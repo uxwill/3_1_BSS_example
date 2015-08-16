c5404c6b8773a5ae6e30750c32e3c1027e9dc155
William <wfsaunders@gmail.com>
2015-08-17 00:24:08 +0200

    got ball bounce to work

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var app = express();
 
+
 app.use(express.static(__dirname +'/public'));
 
 //routes
+
+
 
 app.route('/cool')
     .get(function(req,res){
@@ -20,39 +23,35 @@ console.log(" the server is running on port 3000");
 
     var socketio = require("socket.io");
     var io = socketio.listen(server);
+    var count = 0;
+
 
     io.on('connection',function(socket){
 
     	console.log ("a user has connected");
 
-        //listens to the event name and the callback function that does whatevr we want
+        count++;
 
-    	socket.on('ping', function() {
+        console.log (count);
 
-		console.log("ping received")
+        socket.emit('message',{ count: count});
 
-        //socket talks back on the individual connection, whereas io.emit is everyone that is connected
-
-		socket.emit('pong', "hey y'all message received");
-
-    	});
 
     	socket.on('disconnect', function(){
 
     		console.log("a user has left");
 
+            count --;
+
+            console.log (count);
+
+            socket.emit('message',{ count: count});
+
+
+
     	});
-//this says: when this event happens, do something else. it is linking to changeBackground
-//the event gets the informatoin (data) and then passes it to the call back function
 
-        socket.on('changeBackground', function(data) {
 
-                console.log(data);
-                io.emit('setBackground', data);
-
-                //io.emit sends to everyone connected
-
-        });
 
         socket.emit('initialize', "initialized");
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,93 @@
 $(document).ready(function() {
 
-    var socket = io();
 
-    $('#ping').click(function() {
-        socket.emit('ping', 'Hello Server');
-        console.log('pinged');
+
+
+
+//Bouncing balls code found on: http://jsfiddle.net/jgJsL/5/
+
+    $.fn.bounce = function(options) {
+    
+    var settings = $.extend({
+        speed: 10
+    }, options);
+
+    return $(this).each(function() {
+        
+        var $this = $(this),
+            $parent = $this.parent(),
+            height = $parent.height(),
+            width = $parent.width(),
+            top = Math.floor(Math.random() * (height / 2)) + height / 4,
+            left = Math.floor(Math.random() * (width / 2)) + width / 4,
+            vectorX = settings.speed * (Math.random() > 0.5 ? 1 : -1),
+            vectorY = settings.speed * (Math.random() > 0.5 ? 1 : -1);
+
+        // place initialy in a random location
+        $this.css({
+            'top': top,
+            'left': left
+        }).data('vector', {
+            'x': vectorX,
+            'y': vectorY
+        });
+
+        var move = function($e) {
+            
+            var offset = $e.offset(),
+                width = $e.width(),
+                height = $e.height(),
+                vector = $e.data('vector'),
+                $parent = $e.parent();
+
+            if (offset.left <= 0 && vector.x < 0) {
+                vector.x = -1 * vector.x;
+            }
+            if ((offset.left + width) >= $parent.width()) {
+                vector.x = -1 * vector.x;
+            }
+            if (offset.top <= 0 && vector.y < 0) {
+                vector.y = -1 * vector.y;
+            }
+            if ((offset.top + height) >= $parent.height()) {
+                vector.y = -1 * vector.y;
+            }
+
+            $e.css({
+                'top': offset.top + vector.y + 'px',
+                'left': offset.left + vector.x + 'px'
+            }).data('vector', {
+                'x': vector.x,
+                'y': vector.y
+            });
+            
+            setTimeout(function() {
+                move($e);
+            }, 50);
+            
+        };
+        
+        move($this);
     });
 
-    socket.on('pong', function(data) {
-        console.log("server responed: " + data);
-        $('#serverResponse').append('Server says: ' + data + '</br>');
+};
+
+$(function() {
+
+	var socket = io();
+
+   socket.on('message', function (data) {
+    console.log(data.count);
+
+    
+
+
+    $('#round').bounce({
+        'speed': data.count
     });
 
-    $('#changeBackground').click(function() {
-        //logic to generate random colour in hex
-        var newColour = '#' + Math.floor(Math.random() * 16777215).toString(16);
-        //send to server
-        socket.emit('changeBackground', newColour);
     });
 
-    socket.on('setBackground', function(data) {
-        //wait for server to send colour details in 'setBackground' message then changethe colour to $
-        $('body').css("background-color", data);
-    })
+});
 
 });
